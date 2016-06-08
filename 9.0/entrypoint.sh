bf961ce0615da90b66d7a6367a45bf1653bae334
Andres Pardini <unpardos@gmail.com>
2016-06-08 11:47:55 -0300

    Added gosu and change permissions on odoo file storage folder

--- 9.0/entrypoint.sh
+++ 9.0/entrypoint.sh
@@ -7,18 +7,25 @@ set -e
 : ${PGPORT:=$DB_PORT_5432_TCP_PORT}
 : ${PGUSER:=${DB_ENV_POSTGRES_USER:='postgres'}}
 : ${PGPASSWORD:=$DB_ENV_POSTGRES_PASSWORD}
+
+#Give permissions to odoo if mounted externally
+chown -R odoo /var/lib/odoo
+
 export PGHOST PGPORT PGUSER PGPASSWORD
 
 case "$1" in
-	--)
-		shift
-		exec openerp-server "$@"
-		;;
-	-*)
-		exec openerp-server "$@"
-		;;
-	*)
-		exec "$@"
+    --)
+	shift
+	exec gosu odoo openerp-server "$@"
+	;;
+    -*)
+	exec gosu odoo openerp-server "$@"
+	;;
+    openerp-server)
+	exec gosu odoo "$@"
+	;;
+    *)
+	exec "$@"
 esac
 
 exit 1
